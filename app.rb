b0bbd65c605b947482ff43d4e32f742f3865f5fc
Jen <jenniferanndcb@gmail.com>
2019-08-07 13:52:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,15 +7,15 @@ class App < Sinatra::Base
   end
   
   get '/name' do 
-    "My name is Jen"
+    "My name is "
   end 
   
   get '/hometown' do
-    "My hometown is Batangas"
+    "My hometown is "
   end 
   
   get '/favorite-song' do
-    "My favorite song is Baby"
+    "My favorite song is "
   end 
   
 end
